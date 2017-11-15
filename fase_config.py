cf0071402b60ece2555fdbf302ac2da2142fe235
igushev <igushev@gmail.com>
2017-11-15 02:56:33 -0800

    Cleaning up config modules

--- fase_config.py
+++ fase_config.py
@@ -1,15 +1,9 @@
-import collections
 import os
 import ConfigParser
 
-from boto.dynamodb2.layer1  import DynamoDBConnection
-
 import activation_code_generator
-import sms_sender
 import fase_database
-
-
-DatabaseConfig = collections.namedtuple('DatabaseConfig', ['tables_suffix'])
+import sms_sender
 
 
 def GetConfig(filename):
@@ -18,26 +12,12 @@ def GetConfig(filename):
   return config
 
 
-def GetDynamoDBConnection(config):
-  return DynamoDBConnection(
-      region=config.get('dynamodb', 'region')
-      if config.has_option('dynamodb', 'region') else None,
-      host=config.get('dynamodb', 'host')
-      if config.has_option('dynamodb', 'host') else None,
-      port=config.getint('dynamodb', 'port')
-      if config.has_option('dynamodb', 'port') else None,
-      aws_access_key_id=config.get('dynamodb', 'aws_access_key_id')
-      if config.has_option('dynamodb', 'aws_access_key_id') else None,
-      aws_secret_access_key=config.get('dynamodb', 'aws_secret_access_key')
-      if config.has_option('dynamodb', 'aws_secret_access_key') else None,
-      is_secure=config.getboolean('dynamodb', 'is_secure')
-      if config.has_option('dynamodb', 'is_secure') else None)
-
-
-def GetDatabaseConfig(config):
-  return DatabaseConfig(
-      tables_suffix=config.get('database', 'tables_suffix')
-      if config.has_option('database', 'tables_suffix') else None)
+def GetFaseDatabase(config):
+  return fase_database.DynamoDBFaseDatabase(
+      region_name=config.get('dynamodb', 'region_name'),
+      endpoint_url=config.get('dynamodb', 'endpoint_url'),
+      aws_access_key_id=config.get('dynamodb', 'aws_access_key_id'),
+      aws_secret_access_key=config.get('dynamodb', 'aws_secret_access_key'))
 
 
 def GetActivationCodeGenerator(config):
@@ -54,8 +34,6 @@ def GetSMSSender(config):
     sms_service_provider = sms_sender.NullSMSServiceProvider()
   return sms_sender.SMSSender(
       sms_service_provider=sms_service_provider,
-      ios_link=config.get('sms_sender', 'ios_link'),
-      andriod_link=config.get('sms_sender', 'andriod_link'),
       intercept_to=(
           config.get('sms_sender', 'intercept_to')
           if config.has_option('sms_sender', 'intercept_to')
@@ -64,7 +42,6 @@ def GetSMSSender(config):
 
 
 fase_config = GetConfig('FASE_CONFIG_FILENAME')
+fase_database.FaseDatabase.Set(GetFaseDatabase(fase_config))
 activation_code_generator.ActivationCodeGenerator.Set(GetActivationCodeGenerator(fase_config))
-fase_database.FaseDatabase.Set(fase_database.FaseDatabase(
-    GetDatabaseConfig(fase_config), GetDynamoDBConnection(fase_config)))
 sms_sender.SMSSender.Set(GetSMSSender(fase_config))
--- notes_config.py
+++ notes_config.py
@@ -1,8 +1,14 @@
 import fase_config
-
 import notes_database
 
 
+def GetNotesDatabase(config):
+  return notes_database.DynamoDBNotesDatabase(
+      region_name=config.get('dynamodb', 'region_name'),
+      endpoint_url=config.get('dynamodb', 'endpoint_url'),
+      aws_access_key_id=config.get('dynamodb', 'aws_access_key_id'),
+      aws_secret_access_key=config.get('dynamodb', 'aws_secret_access_key'))
+
+
 notes_config = fase_config.GetConfig('NOTES_CONFIG_FILENAME')
-notes_database.NotesDatabaseInterface.Set(notes_database.NotesDatabaseInterface(
-    fase_config.GetDatabaseConfig(notes_config), fase_config.GetDynamoDBConnection(notes_config)))
+notes_database.NotesDatabaseInterface.Set(GetNotesDatabase(notes_config))
