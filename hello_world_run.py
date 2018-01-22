dfb6b0be3b0ad5f70626654c11a2eb97cdd678b8
igushev <igushev@gmail.com>
2018-01-22 01:31:53 -0800

    Taking session_info from working directory instead of env var

--- hello_world_run.py
+++ hello_world_run.py
@@ -4,14 +4,14 @@ import fase_run
 
 
 FASE_SERVER_URL = 'http://fasehelloworld-env.us-west-2.elasticbeanstalk.com'
-FASE_SESSION_INFO_FILEPATH_VAR = 'FASE_HELLO_WORLD_SESSION_INFO_FILEPATH'
+FASE_SESSION_INFO_FILENAME = 'hello_world_session_info'
 IGNORE_SESSION_INFO = 'ignore'
 RESET_FLAG = 'reset'
 
 
 def main(argv):
   arg_list = [arg[2:] for arg in argv[1:] if arg.startswith('--')]
-  session_info_filepath = (os.environ.get(FASE_SESSION_INFO_FILEPATH_VAR, None)
+  session_info_filepath = (os.path.join(os.getcwd(), FASE_SESSION_INFO_FILENAME)
                            if IGNORE_SESSION_INFO not in arg_list else None)
   if RESET_FLAG in arg_list:
     os.remove(session_info_filepath)
--- notes_run.py
+++ notes_run.py
@@ -4,14 +4,14 @@ import fase_run
 
 
 FASE_SERVER_URL = 'http://fasenotes-env.us-west-2.elasticbeanstalk.com'
-FASE_SESSION_INFO_FILEPATH_VAR = 'FASE_NOTES_SESSION_INFO_FILEPATH'
+FASE_SESSION_INFO_FILENAME = 'notes_session_info'
 IGNORE_SESSION_INFO = 'ignore'
 RESET_FLAG = 'reset'
 
 
 def main(argv):
   arg_list = [arg[2:] for arg in argv[1:] if arg.startswith('--')]
-  session_info_filepath = (os.environ.get(FASE_SESSION_INFO_FILEPATH_VAR, None)
+  session_info_filepath = (os.path.join(os.getcwd(), FASE_SESSION_INFO_FILENAME)
                            if IGNORE_SESSION_INFO not in arg_list else None)
   if RESET_FLAG in arg_list:
     os.remove(session_info_filepath)
