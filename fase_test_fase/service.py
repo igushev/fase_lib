58fca874f6dd88e9d5f6df7eec6ae4776cf58e69
igushev <igushev@gmail.com>
2018-05-07 17:29:33 -0700

    Adding more tests to FaseTest

--- fase_test_fase/service.py
+++ fase_test_fase/service.py
@@ -27,6 +27,9 @@ class FaseTestService(fase.Service):
     return FaseTestService.version
 
   def OnStart(self):
+    return self.StartScreen(None, None)
+
+  def StartScreen(self, screen, element):
     screen = fase.Screen(self)
     screen.SetTitle('FaseTest')
     screen.SetScrollable(True)
@@ -40,6 +43,16 @@ class FaseTestService(fase.Service):
                          on_click=FaseTestService.OnVerticalMaxSizeTest)
     navigation.AddButton(id_='aligning_buttons_test_button', text='Aligning Buttons Test',
                          on_click=FaseTestService.OnAligningButtonsTest)
+    navigation.AddButton(id_='label_and_image_test_button', text='Label And Image Test',
+                         on_click=FaseTestService.OnLabelAndImageTest)
+    navigation.AddButton(id_='web_test_test_button', text='Web Test',
+                         on_click=FaseTestService.OnWebTest)
+    navigation.AddButton(id_='web_and_buttons_max_test_button', text='Web And Buttons Max Test',
+                         on_click=FaseTestService.OnWebAndButtonsMaxTest)
+    navigation.AddButton(id_='web_and_buttons_scrollable_test_button', text='Web And Buttons Scrollable Test',
+                         on_click=FaseTestService.OnWebAndButtonsScrollableTest)
+    navigation.AddButton(id_='error_test_button', text='Error Test',
+                         on_click=FaseTestService.OnErrorTest)
     if self.IfSignedIn():
       navigation.AddButton(id_='sign_out_button', text='Sign Out', on_click=FaseTestService.OnSignOut)
     else:
@@ -51,38 +64,48 @@ class FaseTestService(fase.Service):
     screen.SetScrollable(True)
     self._AddButtons(screen)
     screen.AddLabel(text='Label Left', align=fase.Label.LEFT)
-    screen.AddLabel(text='Label Right', align=fase.Label.RIGHT)
     screen.AddLabel(text='Label Center', align=fase.Label.CENTER)
+    screen.AddLabel(text='Label Right', align=fase.Label.RIGHT)
     screen.AddLabel(text='Text Element Singline')
     screen.AddText(hint='Text Element')
     screen.AddLabel(text='Text Element Multiline')
     screen.AddText(hint='Text Element Multiline', multiline=True)
     screen.AddLabel(text='Switch Element Left')
     screen.AddSwitch(value=True, text='Switch Element', align=fase.Switch.LEFT)
-    screen.AddLabel(text='Switch Element Right')
-    screen.AddSwitch(value=True, text='Switch Element', align=fase.Switch.RIGHT)
     screen.AddLabel(text='Switch Element Center')
     screen.AddSwitch(value=False, text='Switch Element', align=fase.Switch.CENTER)
+    screen.AddLabel(text='Switch Element Right')
+    screen.AddSwitch(value=True, text='Switch Element', align=fase.Switch.RIGHT)
     screen.AddLabel(text='Select Left')
     screen.AddSelect(value='First', items=['First', 'Second', 'Third'], align=fase.Select.LEFT)
-    screen.AddLabel(text='Select Right')
-    screen.AddSelect(value='First', items=['First', 'Second', 'Third'], align=fase.Select.RIGHT)
     screen.AddLabel(text='Select Center')
     screen.AddSelect(value='First', items=['First', 'Second', 'Third'], align=fase.Select.CENTER)
+    screen.AddLabel(text='Select Right')
+    screen.AddSelect(value='First', items=['First', 'Second', 'Third'], align=fase.Select.RIGHT)
     screen.AddLabel(text='Image Filename 128x128')
     screen.AddImage(filename='images/nyc_128x128.jpg')
     screen.AddLabel(text='Image Filename 240x137')
     screen.AddImage(filename='images/nyc_240x137.jpg')
     screen.AddLabel(text='Image Filename 800x600')
     screen.AddImage(filename='images/nyc_800x600.jpg')
+    screen.AddLabel(text='Image Filename 3000x2000')
+    screen.AddImage(filename='images/nyc_national_geographic_3000x2000.jpg')
     screen.AddLabel(text='Image Url Small')
     screen.AddImage(url='http://tryourla.com/wp-content/uploads/2010/08/la_skyline.jpg')
     screen.AddLabel(text='Slider')
     screen.AddSlider(value=50., min_value=0., max_value=200., step=5.)
-    screen.AddLabel(text='Button Text')
-    screen.AddButton(text='Button')
-    screen.AddLabel(text='Button Image')
-    screen.AddButton(image=fase.Image(url='http://tryourla.com/wp-content/uploads/2010/08/la_skyline.jpg'))
+    screen.AddLabel(text='Button Text Left')
+    screen.AddButton(text='Button', align=fase.Label.LEFT)
+    screen.AddLabel(text='Button Text Center')
+    screen.AddButton(text='Button', align=fase.Label.CENTER)
+    screen.AddLabel(text='Button Text Right')
+    screen.AddButton(text='Button', align=fase.Label.RIGHT)
+    screen.AddLabel(text='Button Image Filename 128x128')
+    screen.AddButton(image=fase.Image(filename='images/nyc_128x128.jpg'))
+    screen.AddLabel(text='Button Image Filename 240x137')
+    screen.AddButton(image=fase.Image(filename='images/nyc_240x137.jpg'))
+    screen.AddLabel(text='Button Image Filename 800x600')
+    screen.AddButton(image=fase.Image(filename='images/nyc_800x600.jpg'))
     screen.AddLabel(text='Contact Picker')
     screen.AddContactPicker(hint='Contact Picker')
     screen.AddLabel(text='DateTime Picker Date')
@@ -93,7 +116,7 @@ class FaseTestService(fase.Service):
     screen.AddPlacePicker(hint='Place Picker', type_=fase.PlacePicker.CITY)
     screen.AddSeparator()
     screen.AddLabel(text='Web')
-    screen.AddWeb(url='www.google.com')
+    screen.AddWeb(url='http://www.google.com')
     return screen
 
   def OnVerticalMaxSizeTest(self, screen, element):
@@ -119,7 +142,63 @@ class FaseTestService(fase.Service):
     frame2.AddButton(text='Button1')
     frame2.AddFrame(orientation=fase.Frame.HORIZONTAL, size=fase.Frame.MAX)
     frame2.AddButton(text='Button2')
+    screen.AddLabel(text='Two Buttons on Left Side')
+    frame3 = screen.AddFrame(orientation=fase.Frame.HORIZONTAL)
+    frame3.AddButton(text='Button1')
+    frame3.AddButton(text='Button2')
+    frame3.AddFrame(orientation=fase.Frame.HORIZONTAL, size=fase.Frame.MAX)
     return screen
+
+  def OnLabelAndImageTest(self, screen, element):
+    screen = fase.Screen(self)
+    screen.SetTitle('Label And Image Test')
+    self._AddButtons(screen)
+    frame_outer = screen.AddFrame(id_='frame_outer', orientation=fase.Frame.HORIZONTAL)
+    frame_left = frame_outer.AddFrame(id_='frame_left', orientation=fase.Frame.VERTICAL)
+    frame_left.AddLabel(text='New York City', align=fase.Label.LEFT)
+    frame_left.AddLabel(text='Population: 8 m', align=fase.Label.LEFT)
+    frame_left.AddLabel(text='Rank: 1', align=fase.Label.LEFT)
+    frame_right = frame_outer.AddFrame(id_='frame_right', orientation=fase.Frame.VERTICAL)
+    frame_right.AddImage(filename='images/nyc_800x600.jpg')
+    return screen
+
+  def OnWebTest(self, screen, element):
+    screen = fase.Screen(self)
+    screen.SetTitle('Web Test')
+    self._AddButtons(screen)
+    screen.AddWeb(url='http://www.bmwusa.com', size=fase.Web.MAX)
+    return screen
+
+  def OnWebAndButtonsMaxTest(self, screen, element):
+    screen = fase.Screen(self)
+    screen.SetTitle('Web And Buttons Max Test')
+    screen.AddWeb(url='http://www.bmwusa.com', size=fase.Web.MAX)
+    frame_button = screen.AddFrame(orientation=fase.Frame.HORIZONTAL)
+    frame_button.AddButton(text='Decline', on_click=FaseTestService.StartScreen)
+    frame_button.AddFrame(orientation=fase.Frame.HORIZONTAL, size=fase.Frame.MAX)
+    frame_button.AddButton(text='Agree', on_click=FaseTestService.StartScreen)
+    return screen
+
+  def OnWebAndButtonsScrollableTest(self, screen, element):
+    screen = fase.Screen(self)
+    screen.SetTitle('Web And Buttons Scrollable Test')
+    screen.SetScrollable(True)
+    screen.AddWeb(url='http://www.bmwusa.com')
+    frame_button = screen.AddFrame(orientation=fase.Frame.HORIZONTAL)
+    frame_button.AddButton(text='Decline', on_click=FaseTestService.StartScreen)
+    frame_button.AddFrame(orientation=fase.Frame.HORIZONTAL, size=fase.Frame.MAX)
+    frame_button.AddButton(text='Agree', on_click=FaseTestService.StartScreen)
+    return screen
+    
+  def OnErrorTest(self, screen, element):
+    screen = fase.Screen(self)
+    screen.SetTitle('Error Test')
+    self._AddButtons(screen)
+    screen.AddButton(text='Click to make 500 error', on_click=FaseTestService.MakeError)
+    return screen
+
+  def MakeError(self):
+    pass
 
   def OnSignIn(self, screen, element):
     return fase_sign_in.StartSignIn(
