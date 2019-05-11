99ae2b14b203988d9facd53c4eab3ccc89d7436d
igushev <igushev@gmail.com>
2019-05-10 18:40:07 -0700

    Updating generate_iam_policty.py

--- tools/generate_iam_policy.py
+++ tools/generate_iam_policy.py
@@ -1,3 +1,12 @@
+"""Tool which by given database class which with static GetTableNameList() generates  AWS IAM Policy.
+
+python3 tools/generate_iam_policy.py <module_name> <database_class_name> <tables_suffix> <output_filepath>
+
+Example:
+python3 tools/generate_iam_policy.py fase_server.fase_database DynamoDBFaseDatabase _prod iam_prod.txt
+"""
+
+
 import os
 import sys
 
@@ -36,21 +45,21 @@ END_POLICY="""    ]
 """
 
 
-def GenerateIAMPolicty(module_name, database_class_name, tables_suffix, filepath):
+def GenerateIAMPolicty(module_name, database_class_name, tables_suffix, output_filepath):
   exec('import %s' % module_name)
   module = sys.modules[module_name]
   database_class = getattr(module, database_class_name)
   table_name_list = database_class.GetTableNameList()
   
-  with open(filepath, 'w') as iam_policy_file:
-    iam_policy_file.write(BEGIN_POLICY)
+  with open(output_filepath, 'w') as output_file:
+    output_file.write(BEGIN_POLICY)
     for i, table_name in enumerate(table_name_list):
       table_with_suffix = '%s%s' % (table_name, tables_suffix)
-      iam_policy_file.write(TABLE_POLICY % (table_with_suffix, table_with_suffix), )
+      output_file.write(TABLE_POLICY % (table_with_suffix, table_with_suffix), )
       if i < len(table_name_list) - 1:
-        iam_policy_file.write(',')
-      iam_policy_file.write('\n')
-    iam_policy_file.write(END_POLICY)
+        output_file.write(',')
+      output_file.write('\n')
+    output_file.write(END_POLICY)
 
 
 def main(argv):
@@ -58,8 +67,8 @@ def main(argv):
   module_name = argv[1]
   database_class_name = argv[2]
   tables_suffix = argv[3]
-  filepath = argv[4]
-  GenerateIAMPolicty(module_name, database_class_name, tables_suffix, filepath)
+  output_filepath = argv[4]
+  GenerateIAMPolicty(module_name, database_class_name, tables_suffix, output_filepath)
 
 
 if __name__ == '__main__':
